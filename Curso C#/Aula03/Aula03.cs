f74237c6e70c1dfc49b9516b639a3388826d3fd5
Luiz Guilherme Mariano da Cruz <marianoluizguilherme2@gmail.com>
2020-09-24 18:28:32 -0300

    Segundo Commit teste

---
+++ Curso C#/Aula03/Aula03.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Aula03
+{
+    public static void Main(string[] args)
+    {
+        byte byte1 = 1;
+        int num1 = -32;
+        float num2 = 43.21;
+        char caracter = 'a';
+        string palavra = "Batata";
+    }
+}
