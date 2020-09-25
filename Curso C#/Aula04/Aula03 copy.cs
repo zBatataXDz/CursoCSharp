5d66f2f6a6685d1b1c8b8d2ea27d19b132dbc776
Luiz Guilherme Mariano da Cruz <marianoluizguilherme2@gmail.com>
2020-09-25 18:28:00 -0300

    Aula 4

---
+++ Curso C#/Aula04/Aula03 copy.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Aula03
+{
+    public static void Main(string[] args)
+    {
+        byte byte1 = 1;
+        int numInteiro = -32;
+        float numFloat = 43.2f;
+        char caracter = 'a';
+        string palavra = "Batata";
+    }
+}
